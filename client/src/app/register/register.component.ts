9527b355c6268db98519cc61471d0ade7e6c090b
bulicdanijel <bulicdanijel@gmail.com>
2022-12-16 12:45:07 +0100

    End of section 6

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {}
+
+  constructor(private accountService: AccountService, private toastr: ToastrService) {}
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.accountService.register(this.model).subscribe({
+      next: () => {
+        this.cancel();
+      },
+      error: error => {
+        this.toastr.error(error.error),
+        console.log(error);
+      }
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
 }
